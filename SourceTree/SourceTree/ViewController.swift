61f51cbd363e78c958d229d9b4556ac6b336767a
Al1gazY <al1gazy@mail.ru>
2022-12-14 20:28:43 +0600

    first commit

---
+++ SourceTree/SourceTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SourceTree
+//
+//  Created by Aligazy Kismetov on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
