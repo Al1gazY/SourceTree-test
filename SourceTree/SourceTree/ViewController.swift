7296aff8d339b915b883dc14681f03226500e8c4
Al1gazY <al1gazy@mail.ru>
2022-12-15 21:51:10 +0600

    Test pods

--- SourceTree/SourceTree/ViewController.swift
+++ SourceTree/SourceTree/ViewController.swift
@@ -11,8 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //HIIIII
     }
+    
 
     override func printContent(_ sender: Any?) {
         printContent(Any?)
